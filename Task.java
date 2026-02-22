9516358abc93f33081e468237093f49294cec9c8
Luiza Miranda Gomes <15186@ait.nsw.edu.au>
2026-02-22 15:16:24 +1100

    Add Task class with fields and constructor

--- Task.java
+++ Task.java
@@ -1,1 +1,24 @@
+public class Task {
 
+    private String name;
+    private int priority;
+    private int hours;
+
+    public Task(String name, int priority, int hours) {
+        this.name = name;
+        this.priority = priority;
+        this.hours = hours;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+}
